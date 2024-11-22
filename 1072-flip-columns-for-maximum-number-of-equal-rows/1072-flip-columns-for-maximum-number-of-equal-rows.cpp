fbaf164d6cb418ab8dad35a1296b4a23fdd5ba76
Siao,Yu-Jie <a6194315535@gmail.com>
2024-11-23 01:58:40 +0800

    Time: 19 ms (78.26%), Space: 75.3 MB (59.7%) - LeetHub

---
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        unordered_map<string, int> count;
+        for (const auto &row : matrix) {
+            string rowStr;
+            int first = row[0];
+            for (int r : row) {
+                if (r == first) rowStr += "T";
+                else rowStr += "F";
+            }
+            ++count[rowStr];
+        }
+        int ans = 0;
+        for (auto [k, v] : count) {
+            ans = max(ans, v);
+        }
+        return ans;
+    }
+};
