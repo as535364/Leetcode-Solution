f9ca3db8c59f2c768fdbe82b293d674e42636241
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-04 14:59:55 +0800

    Time: 46 ms (78.65%), Space: 13.7 MB (21.72%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.cpp
+++ 0277-find-the-celebrity/0277-find-the-celebrity.cpp
@@ -4,27 +4,22 @@
 class Solution {
 public:
     int findCelebrity(int n) {
-        unordered_map<int, unordered_map<int, bool>> cache;
         int candidate = 0;
         for (int i = 1; i < n; ++i) {
             if (knows(candidate, i)) {
-                cache[candidate][i] = true;
                 candidate = i;
-            }
-            else {
-                cache[candidate][i] = false;
             }
         }
 
         // to check theLastCandidate doesn't know any other
         for (int i = 0 ; i < n; ++i) {
             if (i == candidate) continue;
-            if ((cache[candidate].count(i) && cache[candidate][i]) || knows(candidate, i)) return -1;
+            if (knows(candidate, i)) return -1;
         }
         // to check everyone knows theLastCandidate
         for (int i = 0 ; i < n; ++i) {
             if (i == candidate) continue;
-            if ((cache[i].count(candidate) && !cache[i][candidate]) || !knows(i, candidate)) return -1;
+            if (!knows(i, candidate)) return -1;
         }
         
         return candidate;
