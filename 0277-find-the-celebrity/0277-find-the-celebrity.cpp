236448d9991d45d97a865e9149d505cec249cfb4
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-04 14:53:17 +0800

    Time: 49 ms (59.2%), Space: 13.7 MB (36.15%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.cpp
+++ 0277-find-the-celebrity/0277-find-the-celebrity.cpp
@@ -4,36 +4,24 @@
 class Solution {
 public:
     int findCelebrity(int n) {
-        unordered_set<int> candidate;
-        for (int i = 0; i < n; ++i) {
-            candidate.insert(i);
-        }
-
-        while (candidate.size() > 1) {
-            int a = *candidate.begin();
-            candidate.erase(a);
-            int b = *candidate.begin();
-            candidate.erase(b);
-            if (knows(a, b)) {
-                candidate.insert(b);
-            }
-            else {
-                candidate.insert(a);
+        int candidate = 0;
+        for (int i = 1; i < n; ++i) {
+            if (knows(candidate, i)) {
+                candidate = i;
             }
         }
 
-        int theLastCandidate = *candidate.begin();
         // to check theLastCandidate doesn't know any other
         for (int i = 0 ; i < n; ++i) {
-            if (i == theLastCandidate) continue;
-            if (knows(theLastCandidate, i)) return -1;
+            if (i == candidate) continue;
+            if (knows(candidate, i)) return -1;
         }
         // to check everyone knows theLastCandidate
         for (int i = 0 ; i < n; ++i) {
-            if (i == theLastCandidate) continue;
-            if (!knows(i, theLastCandidate)) return -1;
+            if (i == candidate) continue;
+            if (!knows(i, candidate)) return -1;
         }
         
-        return theLastCandidate;
+        return candidate;
     }
 };
