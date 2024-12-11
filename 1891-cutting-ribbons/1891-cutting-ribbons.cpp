880beabb14401b863080e76b397c67db61a4ddef
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-11 17:35:22 +0800

    Time: 31 ms (40.68%), Space: 96.1 MB (64.49%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    bool isOK(const vector<int> &ribbons, int k, int x) {
+        int cut = ribbons.size();
+        for (int ribbon : ribbons) {
+            cut += ribbon / x - 1;
+        }
+        return cut >= k;
+    }
+public:
+    int maxLength(vector<int>& ribbons, int k) {
+        int left = 1, right = *max_element(begin(ribbons), end(ribbons));
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isOK(ribbons, k, mid)) {
+                cout << mid << endl;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+};
