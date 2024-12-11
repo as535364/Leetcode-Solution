d8f9907a2f9a6a2e58dbf9a393dce89917cb2a1b
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-11 18:25:07 +0800

    Time: 6 ms (98.79%), Space: 111.5 MB (44.7%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int left = 0, ans = 0;
-        for (int right = 0; right < nums.size(); ++right) {
-            while (nums[right] - nums[left] > 2 * k) left++;
-            ans = max(ans, right - left + 1);
+        int maxNum = *max_element(nums.begin(), nums.end());
+        vector<int> diffArr(maxNum + 2);
+        for (int num : nums) {
+            diffArr[max(0, num - k)]++;
+            diffArr[min(maxNum + 1, num + k + 1)]--;
+        }
+        int soFar = 0, ans = 0;
+        for (int diff : diffArr) {
+            soFar += diff;
+            ans = max(soFar, ans);
         }
         return ans;
     }
