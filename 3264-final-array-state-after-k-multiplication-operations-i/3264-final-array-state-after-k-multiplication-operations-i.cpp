f616631e904f527bba573678dd139e7373967642
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-16 15:43:00 +0800

    Time: 0 ms (100%), Space: 27.9 MB (20.77%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for (int i = 0; i < nums.size(); ++i) {
+            pq.push({nums[i], i});
+        }
+
+        while (k--) {
+            auto [val, idx] = pq.top();
+            pq.pop();
+            nums[idx] = val * multiplier;
+            pq.push({val * multiplier, idx});
+        }
+        return nums;
+    }
+};
