de981a00cbdf63f95ae32e3f20e62c61712e009c
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-17 11:01:04 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        string res;
+        vector<int> cnt(26);
+        for (char c : s) cnt[c - 'a']++;
+
+        priority_queue<pair<char, int>> pq;
+        for (int i = 0; i < 26; ++i) {
+            if (cnt[i]) pq.push({i + 'a', cnt[i]});
+        }
+        
+        char lastChar = '-';
+        int lastCharRepeat = 0;
+
+        while (!pq.empty()) {
+            bool popTwice = false;
+            char repeatChar = '-';
+            int repeatCharCnt = 0;
+            if (pq.top().first == lastChar && lastCharRepeat == repeatLimit) {
+                repeatChar = pq.top().first;
+                repeatCharCnt = pq.top().second;
+                pq.pop();
+                popTwice = true;
+                lastCharRepeat = 0;
+            }
+            if (pq.empty()) break;
+
+            auto [c, cnt] = pq.top();
+            pq.pop();
+            res += c;
+
+            if (popTwice) {
+                pq.push({repeatChar, repeatCharCnt});
+            }
+            lastChar = c;
+            lastCharRepeat++;
+
+            if (cnt > 1) pq.push({c, cnt - 1});
+            else lastCharRepeat = 0;
+        }
+        return res;
+    }
+};
