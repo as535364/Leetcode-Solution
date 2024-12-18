bbd252dd014c7e0e7cad0fbc751fde64aaac8eb4
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-18 17:29:00 +0800

    Time: 0 ms (100%), Space: 14.3 MB (9.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
-        vector<int> ans(prices.size());
-        for (int i = n - 1; i >= 0; --i) {
-            int j = i;
-            for (j = i + 1; j < n; ++j) {
-                if (prices[j] <= prices[i]) break;
+        vector<int> ans = prices;
+        stack<pair<int, int>> st;
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && st.top().second >= prices[i]) {
+                auto [idx, _] = st.top();
+                st.pop();
+                ans[idx] -= prices[i];
             }
-            if (j != n) {
-                ans[i] = prices[i] - prices[j];
-            }
-            else {
-                ans[i] = prices[i];
-            }
+            st.push({i, prices[i]});
         }
         return ans;
     }
