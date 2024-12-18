3962d53e18e752e828d27931b9fd80300046b6f2
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-19 03:11:44 +0800

    Time: 5 ms (90.5%), Space: 162.3 MB (92.07%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,33 +1,17 @@
 class Solution {
 private:
-    const long long minVal = -(1LL << 48);
+    const long long maxVal = 1LL << 48;
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<vector<long long>> groupSum(n - k + 1);
+        vector<long long> preMin(k, maxVal);
+        preMin[k - 1] = 0;
 
-        int left = 0;
-        long long sum = 0;
-        for (int right = 0; right < n; ++right) {
-            sum += nums[right];
-            if (right - left + 1== k) {
-                groupSum[left % k].push_back(sum);
-                sum -= nums[left];
-                left++;
-            }
+        long long res = -maxVal, pre = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            pre += nums[i];
+            res = max(res, pre - preMin[i % k]);
+            preMin[i % k] = min(preMin[i % k], pre);
         }
-
-        long long ans = minVal;
-        for (const auto &g : groupSum) {
-            long long curr = 0;
-            long long groupAns = minVal;
-            for (long long gs : g) {
-                if (curr < 0) curr = gs;
-                else curr += gs;
-                groupAns = max(groupAns, curr);
-            }
-            ans = max(ans, groupAns);
-        }
-        return ans;
+        return res;
     }
 };
