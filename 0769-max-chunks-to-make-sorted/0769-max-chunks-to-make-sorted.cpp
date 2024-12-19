f657ab5832ff5a33d313749ffef6a62bc768f8e6
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-19 15:32:50 +0800

    Time: 0 ms (100%), Space: 9.1 MB (35.22%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -3,11 +3,20 @@ private:
 
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int maxSoFar = -1, ans = 0;
-        for (int i = 0; i < arr.size(); ++i) {
-            maxSoFar = max(maxSoFar, arr[i]);
-            if (maxSoFar == i) ans++;
+        stack<int> st;
+        for (int num : arr) {
+            int maxInSt = INT_MIN;
+            while (!st.empty() && st.top() > num) {
+                maxInSt = max(maxInSt, st.top());
+                st.pop();
+            }
+            if (maxInSt != INT_MIN) {
+                st.push(maxInSt);
+            }
+            else {
+                st.push(num);
+            }
         }
-        return ans;
+        return st.size();
     }
 };
