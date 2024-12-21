9c327790e98335127194fc7f2eee830f65049583
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-22 00:47:42 +0800

    Time: 311 ms (21.43%), Space: 283 MB (7.14%) - LeetHub

--- 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
@@ -14,12 +14,10 @@ private:
     }
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        int len = rectangles.size();
-        vector<vector<int>> xCord(len), yCord(len);
-        for (int i = 0; i < len; ++i) {
-            const auto rectangle = rectangles[i];
-            xCord[i] = {rectangle[0], rectangle[2]};
-            yCord[i] = {rectangle[1], rectangle[3]};
+        vector<vector<int>> xCord, yCord;
+        for (const auto &rectangle : rectangles) {
+            xCord.push_back({rectangle[0], rectangle[2]});
+            yCord.push_back({rectangle[1], rectangle[3]});
         }
         sort(begin(xCord), end(xCord));
         sort(begin(yCord), end(yCord));
