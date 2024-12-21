4436579e7f1d2d5deb06317644e38357147cab68
Siao,Yu-Jie <a6194315535@gmail.com>
2024-12-22 00:36:31 +0800

    Time: 303 ms (21.43%), Space: 283.4 MB (7.14%) - LeetHub

---
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool check(const vector<vector<int>> &cord) {
+        int cnt = 1;
+        int s = cord[0][0], e = cord[0][1];
+
+        for (int i = 1; i < cord.size(); ++i) {
+            if (e <= cord[i][0]) {
+                if(++cnt >= 3) return true;
+            }
+            e = max(cord[i][1], e);
+        }
+        return cnt >= 3;
+    }
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<vector<int>> xCord, yCord;
+        for (const auto &rectangle : rectangles) {
+            xCord.push_back({rectangle[0], rectangle[2]});
+            yCord.push_back({rectangle[1], rectangle[3]});
+        }
+        sort(begin(xCord), end(xCord));
+        sort(begin(yCord), end(yCord));
+        return check(xCord) || check(yCord);
+    }
+};
