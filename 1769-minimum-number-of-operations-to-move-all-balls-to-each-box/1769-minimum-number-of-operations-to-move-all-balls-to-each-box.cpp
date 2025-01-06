ae235250b1c81026c725b16cf4545573f7d10b29
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-06 15:46:34 +0800

    Time: 0 ms (100%), Space: 12.3 MB (40.56%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -5,19 +5,15 @@ public:
         vector<int> ans(n);
         
         int ballsLeft = 0, disLeftSum = 0;
-        for (int i = 0; i < n; ++i) {
+        int ballsRight = 0, disRightSum = 0;
+        for (int i = 0, j = n - 1; i < n; ++i, --j) {
             ans[i] += disLeftSum;
+            ans[j] += disRightSum;
             ballsLeft += (boxes[i] == '1');
             disLeftSum += ballsLeft;
-        }
-
-        int ballsRight = 0, disRightSum = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            ans[i] += disRightSum;
-            ballsRight += (boxes[i] == '1');
+            ballsRight += (boxes[j] == '1');
             disRightSum += ballsRight;
         }
-
         return ans;
     }
 };
