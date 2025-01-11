1b50d00b6890f6383757f17bf076795e957940cc
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-11 22:58:23 +0800

    Time: 4 ms (62.59%), Space: 14.8 MB (66.78%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
+        if (s.length() < k) return false;
+        
         bitset<128> bs;
         for (char c : s) bs.flip(c);
         return bs.count() <= k;
