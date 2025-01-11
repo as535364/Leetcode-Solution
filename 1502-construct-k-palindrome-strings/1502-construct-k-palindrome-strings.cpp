991ff5308851500daee5309df36877df98e8bf7f
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-12 00:31:04 +0800

    Time: 6 ms (54.2%), Space: 15 MB (51.75%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if (s.length() < k) return false;
+        if (k >= 26) return true;
+        
+        bitset<128> bs;
+        for (char c : s) bs.flip(c);
+        return bs.count() <= k;
+    }
+};
