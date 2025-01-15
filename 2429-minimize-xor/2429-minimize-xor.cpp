8807b9177503546a8794fb52badfcae6470ac790
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-15 14:30:24 +0800

    Time: 0 ms (100%), Space: 8.1 MB (10.75%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.cpp
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -3,12 +3,13 @@ private:
     const static int maxBit = 31;
 public:
     int minimizeXor(int num1, int num2) {
-        bitset<maxBit> num1bit(num1), num2bit(num2), numXorBit;
-        int nums2BitCount = num2bit.count();
+        bitset<maxBit> num1bit(num1), numXorBit;
+        int nums2BitCount = __builtin_popcount(num2);
         for (int i = maxBit - 1; i >= 0; --i) {
             if (num1bit[i] && nums2BitCount-- > 0) {
                 numXorBit[i] = true;
             }
+            if (nums2BitCount <= 0) break;
         }
         if (nums2BitCount <= 0) return numXorBit.to_ulong();
 
@@ -16,6 +17,7 @@ public:
             if (!numXorBit[i] && nums2BitCount-- > 0) {
                 numXorBit[i] = true;
             }
+            if (nums2BitCount <= 0) break;
         }
         return numXorBit.to_ulong(); 
     }
