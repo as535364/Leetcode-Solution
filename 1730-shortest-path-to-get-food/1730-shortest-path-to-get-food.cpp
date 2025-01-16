b89fafb364ea64a698665f9d1b2838fe9efe5683
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-16 10:23:25 +0800

    Time: 17 ms (42.93%), Space: 23.4 MB (72.2%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int m, n;
+    bool isValid(const vector<vector<char>>& grid, int x, int y) {
+        return 0 <= x && x < m && 0 <= y && y < n && grid[x][y] != 'X';
+    }
+public:
+    int getFood(vector<vector<char>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '*') {
+                    q.push({i, j});
+                }
+            }
+        }
+
+        vector<vector<bool>> vis(m, vector<bool>(n));
+        int level = 0;
+        while (!q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                auto [x, y] = q.front();
+                q.pop();
+                if (grid[x][y] == '#') return level;
+
+                for (int dirI = 0; dirI < 4; ++dirI) {
+                    int nx = x + dir[dirI][0], ny = y + dir[dirI][1];
+                    if (isValid(grid, nx, ny) && !vis[nx][ny]) {
+                        vis[nx][ny] = true;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+};
