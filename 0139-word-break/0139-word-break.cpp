39ba364fc6c596f90d528d9c6fddba8a4790457b
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-17 16:50:35 +0800

    Time: 8 ms (52.28%), Space: 16 MB (42.32%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        // dp[i] s[0] ~ s[i] can be construct
+        // dp[i] |= dp[i - n] && wordDict.count(n)
+        // if (i - n < 0) return true;
         int n = s.size();
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        vector<bool> dp(n); // dp[i] means s[0] ~ s[i] can be segmented
+        unordered_set<string> wordDictSet(wordDict.begin(), wordDict.end());
+        vector<bool> dp(n);
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j <= i; ++j) {
-                if (j == 0 && words.count(s.substr(j, i - j + 1))) {
-                    dp[i] = true;
-                    break;
-                }
-                else if (words.count(s.substr(j, i - j + 1)) && dp[j - 1]) {
-                    dp[i] = true;
-                    break;
-                }
+            dp[i] = wordDictSet.count(s.substr(0, i + 1));
+            if (dp[i]) continue;
+            for (int j = 0; j < i; ++j) {
+                // s[0] ~ s[j], s[j + 1] ~ s[i]
+                dp[i] = dp[i] || (dp[j] && wordDictSet.count(s.substr(j + 1, i - j)));
+                if (dp[i]) break;
             }
         }
         return dp[n - 1];
