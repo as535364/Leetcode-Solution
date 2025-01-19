ac473bbf300dc7c47769ff4e7fb3f98bfeed623d
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-20 04:25:03 +0800

    Time: 2 ms (92.58%), Space: 13.1 MB (57.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 1e9));
 
-        for (int i = 1; i <= m; ++i) dp[i][0] = i;
-        for (int j = 1; j <= n; ++j) dp[0][j] = j;
+        for (int i = 0; i <= m; ++i) dp[i][0] = i;
+        for (int j = 0; j <= n; ++j) dp[0][j] = j;
 
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
@@ -13,8 +13,9 @@ public:
                     dp[i][j] = dp[i - 1][j - 1];
                 }
                 else {
-                    dp[i][j] = min(dp[i][j - 1] + 1, dp[i - 1][j] + 1); // remove word1[i - 1] or word2[j - 1]
-                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1); // replace word1[i] with word2[j]
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1); // remove word1[i - 1]
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1); // remove word2[j - 1]
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1); // replace word1[i - 1] with word2[j - 1]
                 }
             }
         }
