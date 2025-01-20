3bbc67e67e1fb23bca5a951ac5ce46a98bda34cb
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-21 00:34:28 +0800

    Time: 14 ms (86.85%), Space: 9.1 MB (87.05%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
+    /* O(mn) O(mn)
     int numDistinct(string s, string t) {
         int m = s.size(), n = t.size();
         vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1));
         // dp[i][j] means s[0...i-1] t[0...j-1] distinct subsequences
-        // s[i - 1] == t[j - 1], dp[i][j] += dp[i - 1][j - 1]
 
         for (int j = 0; j <= n; ++j) dp[0][j] = 0;
         for (int i = 0; i <= m; ++i) dp[i][0] = 1;
@@ -16,5 +16,25 @@ public:
             }
         }
         return dp[m][n];
+    }
+    */
+
+    // O(mn) O(n)
+    int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        vector<unsigned long long> dp(n + 1), prev(n + 1);
+        // dp[i][j] means s[0...i-1] t[0...j-1] distinct subsequences
+
+        prev[0] = 1;
+        for (int i = 1; i <= m; ++i) {
+            fill(dp.begin(), dp.end(), 0);
+            dp[0] = 1;
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == t[j - 1]) dp[j] += prev[j - 1] + prev[j];
+                else dp[j] = prev[j];
+            }
+            prev = dp;
+        }
+        return dp[n];
     }
 };
