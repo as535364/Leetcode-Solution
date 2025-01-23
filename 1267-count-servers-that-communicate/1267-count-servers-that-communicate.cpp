d1b2e247ded31ac858ead772fd9ef4d6daa1266e
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-23 10:34:36 +0800

    Time: 0 ms (100%), Space: 26.6 MB (73.49%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,30 +1,21 @@
 class Solution {
-    int m, n;
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-        grid[r][c] = 0;
-        int communicateCount = 0;
-        for (int i = 0; i < n; ++i) {
-            if (grid[r][i]) {
-                communicateCount += dfs(grid, r, i);
-            }
-        }
-        for (int i = 0; i < m; ++i) {
-            if (grid[i][c]) {
-                communicateCount += dfs(grid, i, c);
-            }
-        }
-        return communicateCount + 1;
-    }
 public:
     int countServers(vector<vector<int>>& grid) {
-        m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size();
+        vector<int> rowCount(m), colCount(n);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (!grid[i][j]) continue;
+                ++rowCount[i], ++colCount[j];
+            }
+        }
 
         int res = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (!grid[i][j]) continue;
-                int count = dfs(grid, i, j);
-                if (count > 1) res += count;
+                if (rowCount[i] > 1 || colCount[j] > 1) res++;
             }
         }
         return res;
