6ffba2dfeb0882c723dce470023b26eca900dae6
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-28 17:14:20 +0800

    Time: 4 ms (78.63%), Space: 94 MB (85.64%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -2,13 +2,13 @@ class Solution {
 private:
     int dir[4][2]= {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     int m, n;
-    int dfs(vector<vector<int>>& grid, int r, int c, int sum) {
-        sum += grid[r][c];
+    int dfs(vector<vector<int>>& grid, int r, int c) {
+        int sum = grid[r][c];
         grid[r][c] = 0;
         for (auto &d : dir) {
             int nr = r + d[0], nc = c + d[1];
             if (0 <= nr && nr < m && 0 <= nc && nc < n && grid[nr][nc] > 0) {
-                sum += dfs(grid, nr, nc, 0);
+                sum += dfs(grid, nr, nc);
             }
         }
         return sum;
@@ -20,7 +20,7 @@ public:
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 0) continue;
-                res = max(res, dfs(grid, i, j, 0));
+                res = max(res, dfs(grid, i, j));
             }
         }
         return res;
