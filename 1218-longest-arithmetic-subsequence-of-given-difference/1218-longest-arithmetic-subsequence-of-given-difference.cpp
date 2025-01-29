781a6043597aa69ecad2481b723bdd704b99973b
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-29 21:21:00 +0800

    Time: 51 ms (25.6%), Space: 58.8 MB (76.79%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
         int n = arr.size(), ans = 1;
-        unordered_map<int, int> dp, visIdx;
-        for (int num : arr) dp[num] = 1;
-
+        unordered_map<int, int> dp;
         for (int i = 0; i < n; ++i) {
-            int prevTarget = arr[i] - difference;
-            if (!visIdx.count(prevTarget)) {
-                visIdx[arr[i]] = i;
+            int target = arr[i] - difference;
+            if (!dp.count(target)) {
+                if (!dp.count(arr[i])) dp[arr[i]] = 1;
                 continue;
             }
-
-            if (dp[arr[i]] < dp[prevTarget] + 1) {
-                dp[arr[i]] = dp[prevTarget] + 1;
+            if (dp[arr[i]] < dp[target] + 1) {
+                dp[arr[i]] = dp[target] + 1;
             }
-            visIdx[arr[i]] = i;
             ans = max(ans, dp[arr[i]]);
+            if (!dp.count(arr[i])) dp[arr[i]] = 1;
         }
         return ans;
     }
