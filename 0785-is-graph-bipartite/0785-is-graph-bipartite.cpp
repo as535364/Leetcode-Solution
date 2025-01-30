7d26e728d0cc84a3dcd8b5128c75d4b3fff3d8a2
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-31 00:58:03 +0800

    Time: 0 ms (100%), Space: 17.5 MB (17.77%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -5,16 +5,16 @@ public:
         vector<int> color(n, -1);
         for (int i = 0; i < n; ++i) {
             if (color[i] != -1) continue;
-            queue<int> q;
-            q.push(i);
+            stack<int> st;
+            st.push(i);
             color[i] = 1;
-            while (!q.empty()) {
-                int node = q.front(); q.pop();
+            while (!st.empty()) {
+                int node = st.top(); st.pop();
                 for (int neighbor : graph[node]) {
                     if (color[neighbor] == color[node]) return false;
                     if (color[neighbor] == -1) {
                         color[neighbor] = 1 ^ color[node];
-                        q.push(neighbor);
+                        st.push(neighbor);
                     }
                 }
             }
