0cf9c2e62717577c71550f0ce6b1c90d3eb83611
Siao,Yu-Jie <a6194315535@gmail.com>
2025-01-31 18:07:03 +0800

    Time: 293 ms (37.33%), Space: 159.3 MB (39.72%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,71 @@
+class DSU {
+private:
+    int n;
+    vector<int> parent, rank;
+public:
+    DSU (int n): n(n), parent(n), rank(n, 1) {
+        for (int i = 0; i < n; ++i) parent[i] = i;
+    }
+    int find (int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+    bool merge(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return false;
+        if (rank[x] > rank[y]) swap(x, y);
+        rank[y] += rank[x];
+        parent[x] = y;
+        return true;
+    }
+    int findSize(int x) {
+        x = find(x);
+        return rank[x];
+    }
+};
+
+class Solution {
+private:
+    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    bool isValid(int nr, int nc, int m, int n, const vector<vector<int>>& grid) {
+        return 0 <= nr && nr < m && 0 <= nc && nc < n && grid[nr][nc];
+    }
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        DSU dsu(m * n);
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (!grid[r][c]) continue;
+                for (auto &d : dir) {
+                    int nr = r + d[0], nc = c + d[1];
+                    if (!isValid(nr, nc, m, n, grid)) continue;
+                    dsu.merge(r * m + c, nr * m + nc);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c]) {
+                    res = max(res, dsu.findSize(r * m + c));
+                    continue;
+                }
+                unordered_set<int> uniqueRoot;
+                for (auto &d : dir) {
+                    int nr = r + d[0], nc = c + d[1];
+                    if (!isValid(nr, nc, m, n, grid)) continue;
+                    int nowRoot = dsu.find(nr * m + nc);
+                    if (!uniqueRoot.count(nowRoot)) {
+                        uniqueRoot.insert(nowRoot);
+                    }
+                }
+                int sum = 1;
+                for (int root : uniqueRoot) sum += dsu.findSize(root);
+                res = max(res, sum);
+            }
+        }
+        return res;
+    }
+};
