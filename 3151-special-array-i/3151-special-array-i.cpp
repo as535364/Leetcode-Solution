b2b3a45a3b5b410204ed0b5df1558cdf134b3091
Siao,Yu-Jie <a6194315535@gmail.com>
2025-02-01 15:33:47 +0800

    Time: 0 ms (100%), Space: 27.8 MB (25%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         for (int i = 0; i + 1 < nums.size(); ++i) {
-            if (((nums[i] ^ nums[i + 1]) & 1) == 0) return false;
+            if (!((nums[i] ^ nums[i + 1]) & 1)) return false;
         }
         return true;
     }
