a6793d0ac94a72ab1802a75a99a0513dd5c43d0b
Siao,Yu-Jie <a6194315535@gmail.com>
2025-02-03 04:15:04 +0800

    Time: 36 ms (88%), Space: 81.4 MB (99.25%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -8,12 +8,10 @@ public:
         });
 
         vector<int> res;
-        vector<int> secondDim;
-        for (int i = 0; i < n; ++i) secondDim.push_back(envelopes[i][1]);
 
         for (int i = 0; i < n; ++i) {
-            if (res.empty() || res.back() < secondDim[i]) res.push_back(secondDim[i]);
-            else *lower_bound(res.begin(), res.end(), secondDim[i]) = secondDim[i];
+            if (res.empty() || res.back() < envelopes[i][1]) res.push_back(envelopes[i][1]);
+            else *lower_bound(res.begin(), res.end(), envelopes[i][1]) = envelopes[i][1];
         }
         return res.size();
     }
