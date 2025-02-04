c5ced214b8f3fe8b48182ac1ff8ac88e5ce7fff4
Siao,Yu-Jie <a6194315535@gmail.com>
2025-02-05 03:09:58 +0800

    Time: 3 ms (35.82%), Space: 16.1 MB (61.06%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size(), ans = 0;
-        vector<vector<int>> dp(n, vector<int>(2, -5000005));
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
-
-        for (int i = 1; i < n; ++i) {
-            dp[i][0] = -prices[i];
-            for (int j = 0; j < i - 1; ++j) {
-                dp[i][0] = max(dp[i][0], dp[j][1] - prices[i]); // buy
-                dp[i][1] = max(dp[i][1], dp[j][0] + prices[i]); // sell
+        int n = prices.size();
+        vector<vector<int>> dp(n + 2, vector<int>(2));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int holding = 0; holding < 2; ++holding) {
+                dp[i][holding] = dp[i + 1][holding]; // skip
+                if (holding) { // sell
+                    dp[i][holding] = max(dp[i][holding], prices[i] + dp[i + 2][0]);
+                }
+                else { // buy
+                    dp[i][holding] = max(dp[i][holding], -prices[i] + dp[i + 1][1]);
+                }
             }
-            dp[i][1] = max(dp[i][1], dp[i - 1][0] + prices[i]);
-            ans = max(ans, max(dp[i][0], dp[i][1]));
         }
-        return ans;
+        return dp[0][0];
     }
-    // dp[i][0] means buy at day i
-    // dp[i][0] = max(dp[j][1]) - prices[i] for all 0 <= j < i - 1
-    // dp[i][1] means sell at day i
-    // dp[i][1] = max(dp[j][0]) + prices[i] for all 0 <= j < i
 };
