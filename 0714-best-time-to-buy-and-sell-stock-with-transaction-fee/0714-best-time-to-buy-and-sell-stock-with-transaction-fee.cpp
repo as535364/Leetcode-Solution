7fd5307010e1ba53c0eda45c9b81a15b9d832cdf
Siao,Yu-Jie <a6194315535@gmail.com>
2025-02-05 17:59:47 +0800

    Time: 0 ms (100%), Space: 63.5 MB (67.79%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<int> sold(n + 1, -1e9), reset(n + 1, -1e9), held(n + 1, -1e9);
-        reset[0] = 0;
+        vector<int> hold(n + 1, -1e9), free(n + 1, -1e9);
+        free[0] = 0;
         for (int i = 1; i <= n; ++i) {
             int price = prices[i - 1];
-            held[i] = max(held[i - 1], max(sold[i - 1], reset[i - 1]) - price);
-            reset[i] = max(reset[i - 1], sold[i - 1]);
-            sold[i] = held[i - 1] + price - fee;
+            hold[i] = max(hold[i - 1], free[i - 1] - price);
+            free[i] = max(free[i - 1], hold[i - 1] + price - fee);
         }
-        return max(sold[n], reset[n]);
+        return free[n];                                              
     }
 };
-
-// - Buying at prices[0] = 1
-// - Selling at prices[3] = 8
-// - Buying at prices[4] = 4
-// - Selling at prices[5] = 9
-// The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
