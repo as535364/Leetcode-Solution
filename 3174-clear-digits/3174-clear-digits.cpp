3dd412025e572ba6228e8648d73a5f99bb07e09a
Siao,Yu-Jie <a6194315535@gmail.com>
2025-02-11 16:09:02 +0800

    Time: 0 ms (100%), Space: 8.2 MB (96.32%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -3,7 +3,7 @@ public:
     string clearDigits(string s) {
         string res;
         for (char c : s) {
-            if (isdigit(c) && !res.empty()) res.pop_back();
+            if (isdigit(c)) res.pop_back();
             else res.push_back(c);
         }
         return res;
