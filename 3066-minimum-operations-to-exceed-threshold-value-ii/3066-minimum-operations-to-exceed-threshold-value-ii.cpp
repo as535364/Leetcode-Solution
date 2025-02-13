a8ac57d5f66ba5ca5a891086a533b9c787d5c7bf
Siao,Yu-Jie <a6194315535@gmail.com>
2025-02-13 11:05:41 +0900

    Time: 96 ms (59.95%), Space: 99 MB (39.26%) - LeetHub

---
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for (int num : nums) pq.push(num);
+
+        int operationCnt = 0;
+        while (pq.top() < k) {
+            long long x = pq.top(); pq.pop();
+            long long y = pq.top(); pq.pop();
+            pq.push(x * 2 + y);
+            operationCnt++;
+        }
+        return operationCnt;
+    }
+};
