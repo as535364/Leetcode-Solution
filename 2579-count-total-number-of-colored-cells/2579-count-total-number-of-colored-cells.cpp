20757793870a5f7099fa72cf6c2ca26df68df747
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-05 17:24:31 +0800

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long coloredCells(int n) {
         if (n == 1) return 1;
-        if (n >= 6000) return 1 + 2 * ((long long)n * (n - 1));
+        if (n >= 7000) return 1 + 2 * ((long long)n * (n - 1));
         long long ans = 1, last = 0;
         for (int i = 1; i < n; ++i) {
             last += 4;
