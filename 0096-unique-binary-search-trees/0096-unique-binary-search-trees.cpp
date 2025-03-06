43088c4b72bea18536894dfb1626b0b8df89e785
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-06 19:52:42 +0800

    Time: 0 ms (100%), Space: 7.9 MB (60.96%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> dp(n + 1, 1);
+        // long C = 1;
+        // for (int i = 2; i <= n; ++i) {
+        //     C = 2 * (2 * (i - 1) + 1) * C / (i + 1);
+        // }
+        // return (int)C;
+        vector<int> dp(n + 1);
+        dp[0] = dp[1] = 1;
         for (int i = 2; i <= n; ++i) {
-            dp[i] = 2LL * (2 * (i - 1) + 1) * dp[i - 1] / (i + 1);
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
         }
         return dp[n];
     }
