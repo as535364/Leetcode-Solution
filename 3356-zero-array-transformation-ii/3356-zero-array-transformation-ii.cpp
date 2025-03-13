4066ceb42a586a70d1a90e0d85615b4f72384355
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-13 19:38:42 +0800

    Time: 70 ms (43.15%), Space: 346.1 MB (28.94%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
@@ -2,12 +2,12 @@ class Solution {
 private:
     bool isOK(const vector<int>& nums, const vector<vector<int>>& queries, int k) {
         int n = nums.size();
-        vector<int> prefix(n);
+        vector<int> prefix(n + 1);
         for (int i = 0; i < k; ++i) {
             const auto &query = queries[i];
             int l = query[0], r = query[1], val = query[2];
             prefix[l] += val;
-            if (r + 1 < n) prefix[r + 1] -= val;
+            prefix[r + 1] -= val;
         }
         // for (int i = 0; i < n; ++i) cout << prefix[i] << ' ';
         // cout << endl;
