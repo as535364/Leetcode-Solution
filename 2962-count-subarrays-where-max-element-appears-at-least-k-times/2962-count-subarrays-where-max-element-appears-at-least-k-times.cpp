8b7bdd6e2a61076c2b2cd90029400e9f0fe0c974
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-13 21:42:24 +0800

    Time: 5 ms (55.02%), Space: 121.7 MB (75.71%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -6,8 +6,8 @@ public:
 
         int left = 0;
 
-        for (int right = 0; right < nums.size(); ++right) {
-            if (nums[right] == target) ++targetCount;
+        for (int num : nums) {
+            if (num == target) ++targetCount;
             while (targetCount >= k) {
                 if (nums[left] == target) --targetCount;
                 ++left;
