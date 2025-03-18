73d79d17b82c0f6fbb6d43a9bd1eeb3d3a924295
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-18 19:33:26 +0800

    Time: 0 ms (100%), Space: 61 MB (28.44%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,37 +1,16 @@
 class Solution {
-private:
-    // return true means nice
-    bool add(vector<int> &cnt, int x) {
-        bool nice = true;
-        for (int i = 0; i < 32; ++i) {
-            if (x & 1) cnt[i]++;
-            if (cnt[i] > 1) nice = false;
-            x >>= 1;
-        }
-        return nice;
-    }
-    bool remove(vector<int> &cnt, int x) {
-        bool nice = true;
-        for (int i = 0; i < 32; ++i) {
-            if (x & 1) cnt[i]--;
-            nice = nice && (cnt[i] <= 1);
-            x >>= 1;
-        }
-        return nice;
-    }
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        vector<int> cnt(32);
-        int res = 0, left = 0;
-
+        int usedBits = 0, ans = 1;
+        int left = 0;
         for (int right = 0; right < nums.size(); ++right) {
-            bool nice = add(cnt, nums[right]);
-            while (!nice) {
-                nice = remove(cnt, nums[left]);
+            while (usedBits & nums[right]) {
+                usedBits ^= nums[left];
                 left++;
             }
-            res = max(res, right - left + 1);
+            usedBits |= nums[right];
+            ans = max(ans, right - left + 1);
         }
-        return res;
+        return ans;
     }
 };
