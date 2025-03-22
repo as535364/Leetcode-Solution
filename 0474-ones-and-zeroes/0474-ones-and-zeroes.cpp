739dd09bc35bc63a153f91541dfa2924b774dd45
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-23 04:08:58 +0800

    Time: 53 ms (75.66%), Space: 13.9 MB (66.27%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,27 +1,9 @@
 class Solution {
-private:
-    vector<vector<vector<int>>> dp;
-    int topDown(int idx, int m, int n, vector<pair<int, int>> &s) {
-        if (m == 0 && n == 0) return 0;
-        if (idx == s.size()) return 0;
-        if (dp[idx][m][n] != -1) return dp[idx][m][n];
-
-        int zeroCnt = s[idx].first, oneCnt = s[idx].second;
-        if (m < zeroCnt || n < oneCnt) {
-            dp[idx][m][n] = topDown(idx + 1, m, n, s);
-        }
-        else {
-            int notPick = topDown(idx + 1, m, n, s);
-            int pick = topDown(idx + 1, m - zeroCnt, n - oneCnt, s) + 1;
-            dp[idx][m][n] = max(pick, notPick);
-        }
-        return dp[idx][m][n];
-    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
         vector<pair<int, int>> s;
-        vector<vector<vector<int>>> dp(len + 1, vector<vector<int>>(m + 1, vector<int>(n + 1)));
+        vector<vector<int>> dp(vector<vector<int>>(m + 1, vector<int>(n + 1)));
 
         for (string &str : strs) {
             int zeroCnt = 0, oneCnt = 0;
@@ -34,19 +16,12 @@ public:
     
         for (int i = 1; i <= len; ++i) {
             int zeroCnt = s[i - 1].first, oneCnt = s[i - 1].second;
-            for (int j = 0; j <= m; ++j) {
-                for (int k = 0; k <= n; ++k) {
-                    int notPick = dp[i - 1][j][k];
-                    if (j < zeroCnt || k < oneCnt) {
-                        dp[i][j][k] = notPick;
-                    }
-                    else {
-                        int pick = dp[i - 1][j - zeroCnt][k - oneCnt] + 1;
-                        dp[i][j][k] = max(notPick, pick);
-                    }
+            for (int j = m; j >= zeroCnt; --j) {
+                for (int k = n; k >= oneCnt; --k) {
+                    dp[j][k] = max(dp[j][k], dp[j - zeroCnt][k - oneCnt] + 1);
                 }
             }
         }
-        return dp[len][m][n];
+        return dp[m][n];
     }
 };
