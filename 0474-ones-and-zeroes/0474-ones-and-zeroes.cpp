5acb726d9492324d189e3132cc71ed1fd58e345f
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-23 04:10:03 +0800

    Time: 165 ms (42.57%), Space: 106 MB (31.96%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,9 +1,25 @@
 class Solution {
+private:
+    vector<vector<vector<int>>> dp;
+    int topDown(int idx, int m, int n, vector<pair<int, int>> &s) {
+        if (m == 0 && n == 0) return 0;
+        if (idx == s.size()) return 0;
+        if (dp[idx][m][n] != -1) return dp[idx][m][n];
+
+        int zeroCnt = s[idx].first, oneCnt = s[idx].second;
+        if (m < zeroCnt || n < oneCnt) {
+            dp[idx][m][n] = topDown(idx + 1, m, n, s);
+        }
+        else {
+            dp[idx][m][n] = max(topDown(idx + 1, m, n, s), topDown(idx + 1, m - zeroCnt, n - oneCnt, s) + 1);
+        }
+        return dp[idx][m][n];
+    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
         vector<pair<int, int>> s;
-        vector<vector<int>> dp(vector<vector<int>>(m + 1, vector<int>(n + 1)));
+        dp = vector<vector<vector<int>>>(len, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
 
         for (string &str : strs) {
             int zeroCnt = 0, oneCnt = 0;
@@ -13,15 +29,6 @@ public:
             }
             s.push_back({zeroCnt, oneCnt});
         }
-    
-        for (int i = 1; i <= len; ++i) {
-            int zeroCnt = s[i - 1].first, oneCnt = s[i - 1].second;
-            for (int j = m; j >= zeroCnt; --j) {
-                for (int k = n; k >= oneCnt; --k) {
-                    dp[j][k] = max(dp[j][k], dp[j - zeroCnt][k - oneCnt] + 1);
-                }
-            }
-        }
-        return dp[m][n];
+        return topDown(0, m, n, s);
     }
 };
