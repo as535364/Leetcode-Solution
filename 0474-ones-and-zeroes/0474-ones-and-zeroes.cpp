17054178f15bfffc596a36b1c26fad61b3db50fb
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-23 04:05:15 +0800

    Time: 208 ms (23.7%), Space: 106.8 MB (16.73%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -11,7 +11,9 @@ private:
             dp[idx][m][n] = topDown(idx + 1, m, n, s);
         }
         else {
-            dp[idx][m][n] = max(topDown(idx + 1, m, n, s), topDown(idx + 1, m - zeroCnt, n - oneCnt, s) + 1);
+            int notPick = topDown(idx + 1, m, n, s);
+            int pick = topDown(idx + 1, m - zeroCnt, n - oneCnt, s) + 1;
+            dp[idx][m][n] = max(pick, notPick);
         }
         return dp[idx][m][n];
     }
@@ -19,7 +21,7 @@ public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
         vector<pair<int, int>> s;
-        dp = vector<vector<vector<int>>>(len, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
+        vector<vector<vector<int>>> dp(len + 1, vector<vector<int>>(m + 1, vector<int>(n + 1)));
 
         for (string &str : strs) {
             int zeroCnt = 0, oneCnt = 0;
@@ -29,6 +31,22 @@ public:
             }
             s.push_back({zeroCnt, oneCnt});
         }
-        return topDown(0, m, n, s);
+    
+        for (int i = 1; i <= len; ++i) {
+            int zeroCnt = s[i - 1].first, oneCnt = s[i - 1].second;
+            for (int j = 0; j <= m; ++j) {
+                for (int k = 0; k <= n; ++k) {
+                    int notPick = dp[i - 1][j][k];
+                    if (j < zeroCnt || k < oneCnt) {
+                        dp[i][j][k] = notPick;
+                    }
+                    else {
+                        int pick = dp[i - 1][j - zeroCnt][k - oneCnt] + 1;
+                        dp[i][j][k] = max(notPick, pick);
+                    }
+                }
+            }
+        }
+        return dp[len][m][n];
     }
 };
