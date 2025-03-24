d25ff179d0b40742f3afac43ef77c675b3012398
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-24 21:14:08 +0800

    Time: 10 ms (55.53%), Space: 11.1 MB (41.5%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
-        vector<int> nextA(n, n), nextB(n, n), nextC(n, n);
-        int nextAIdx = n, nextBIdx = n, nextCIdx = n;
-
-        for (int i = n - 1; i >= 0; --i) {
-            nextA[i] = nextAIdx;
-            nextB[i] = nextBIdx;
-            nextC[i] = nextCIdx;
-            if (s[i] == 'a') nextAIdx = i;
-            else if (s[i] == 'b') nextBIdx = i;
-            else if (s[i] == 'c') nextCIdx = i;
-        }
-
-        int res = 0;
+        int n = s.size(), res = 0;
+        vector<int> lastPos(3, -1);
         for (int i = 0; i < n; ++i) {
-            int minEnd = -1;
-            if (s[i] == 'a') {
-                minEnd = max(nextB[i], nextC[i]);
-            }
-            else if (s[i] == 'b') {
-                minEnd = max(nextA[i], nextC[i]);
-            }
-            else if (s[i] == 'c') {
-                minEnd = max(nextA[i], nextB[i]);
-            }
-            res += (n - minEnd);
+            lastPos[s[i] - 'a'] = i;
+            res += 1 + min({lastPos[0], lastPos[1], lastPos[2]});
         }
         return res;
     }
