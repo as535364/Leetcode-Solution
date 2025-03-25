4306f35d518cf40ee7dfca6cf32d922f70a5d387
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-26 02:57:27 +0800

    Time: 5 ms (66.9%), Space: 11.6 MB (87.9%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,19 +1,20 @@
 class Solution {
-    const int MOD = 1e9 + 7;
+private:
+    const int mod = 1e9 + 7;
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        int ans = 0;
         vector<int> dp(high + 1);
-        dp[0] = 1;
-        for (int i = 1; i <= high; ++i) {
-            if (i >= zero) dp[i] = dp[i - zero];
+        dp[zero] += 1;
+        dp[one] += 1;
+
+        int res = 0;
+        for (int i = min(zero, one); i <= high; ++i) {
+            if (i >= zero) dp[i] += dp[i - zero];
             if (i >= one) dp[i] += dp[i - one];
-            dp[i] %= MOD;
-            if (low <= i && i <= high) {
-                ans += dp[i];
-                ans %= MOD;
-            }
+            dp[i] %= mod;
+            if (low <= i && i <= high) res += dp[i];
+            res %= mod;
         }
-        return ans;
+        return res;
     }
 };
