db5b34095a9e2693691d920324cf2cb9295e1606
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-27 02:20:24 +0800

    Time: 0 ms (100%), Space: 13.4 MB (30.61%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,41 +1,23 @@
 class Solution {
+private:
+    const int inf = 1e7;
 public:
-    unordered_set<int> isTravelNeeded;
-    
-    int solve(vector<int>& dp, vector<int>& days, vector<int>& costs, int currDay) {
-        // If we have iterated over travel days, return 0.
-        if (currDay > days[days.size() - 1]) {
-            return 0;
-        }
-        
-        // If we don't need to travel on this day, move on to next day.
-        if (isTravelNeeded.find(currDay) == isTravelNeeded.end()) {
-            return solve(dp, days, costs, currDay + 1);
-        }
-        
-        // If already calculated, return from here with the stored answer.
-        if (dp[currDay] != -1) {
-            return dp[currDay];
-        }
-        
-        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
-        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
-        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
-        
-        // Store the cost with the minimum of the three options.
-        return dp[currDay] = min(oneDay, min(sevenDay, thirtyDay));
-    }
-    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        // The last day on which we need to travel.
-        int lastDay = days[days.size() - 1];
-        vector<int> dp(lastDay + 1, -1);
-        
-        // Mark the days on which we need to travel.
-        for (int day : days) {
-            isTravelNeeded.insert(day);
+        int n = days.size();
+        days.push_back(400);
+        vector<int> dp(n + 1, inf);
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            int Day1Idx = i + 1;
+            int Day7Idx = lower_bound(days.begin(), days.end(), days[i] + 7) - days.begin();
+            int Day30Idx = lower_bound(days.begin(), days.end(), days[i] + 30) - days.begin();
+
+            int buy1Cost = dp[Day1Idx] + costs[0];
+            int buy7Cost = dp[Day7Idx] + costs[1];
+            int buy30Cost = dp[Day30Idx] + costs[2];
+            dp[i] = min({dp[i], buy1Cost, buy7Cost, buy30Cost});
         }
-        
-        return solve(dp, days, costs, 1);
+        return dp[0];
     }
 };
