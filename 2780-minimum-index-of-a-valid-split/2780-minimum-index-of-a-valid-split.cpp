026f6e97a36f2c6df4304fb235f14a234dd08d87
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-27 15:05:48 +0800

    Time: 4 ms (77.46%), Space: 89.9 MB (85.66%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> cnt;
-        for (int num : nums) ++cnt[num];
         
-        int dominant = -1;
-        for (auto [num, c] : cnt) {
-            if (c > n / 2) {
+        int dominant = -1, dominantCnt = 1;
+        for (int num : nums) {
+            if (dominant == num) {
+                dominantCnt++;
+            }
+            else {
+                dominantCnt--;
+            }
+            if (dominantCnt == 0) {
                 dominant = num;
-                break;
+                dominantCnt = 1;
             }
         }
 
-        vector<int> prefix(n);
-        prefix[0] = (nums[0] == dominant);
-        for (int i = 1; i < n; ++i) {
-            prefix[i] = prefix[i - 1] + (nums[i] == dominant);
-        }
+        int xCnt = 0;
+        for (int num : nums) xCnt += num == dominant;
         
+        int nowXCnt = 0;
         for (int i = 0; i < n; ++i) {
+            nowXCnt += nums[i] == dominant;
             int leftPartLen = i + 1, rightPartLen = n - i - 1;
-            int leftPartDominant = prefix[i], rightPartDominant = prefix[n - 1] - prefix[i];
+            int leftPartDominant = nowXCnt, rightPartDominant = xCnt - nowXCnt;
             if (leftPartDominant > leftPartLen / 2 && rightPartDominant > rightPartLen / 2) return i;
         }
         return -1;
