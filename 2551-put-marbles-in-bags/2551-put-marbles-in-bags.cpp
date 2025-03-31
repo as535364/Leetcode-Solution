9e9c3268cfe562bb21879dc867a3a75068a1122c
Siao,Yu-Jie <a6194315535@gmail.com>
2025-03-31 23:29:55 +0800

    Time: 30 ms (91.14%), Space: 64.1 MB (91.14%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
         int n = weights.size();
-        if(k == 1 || k == n)return 0;
-        vector<int> bars;
-        for(int i = 0; i + 1 < n; ++i){
-            bars.push_back(weights[i] + weights[i + 1]);
+        vector<int> possibleCost(n - 1);
+        for (int i = 1; i < n; ++i) {
+            possibleCost[i - 1] = weights[i - 1] + weights[i];
         }
-        sort(bars.begin(), bars.end());
-        long long maxAns = 0, minAns = 0;
-        for(int i = 0; i < k - 1; ++i){
-            maxAns += bars[n - 2 - i];
-            minAns += bars[i];
+        sort(possibleCost.begin(), possibleCost.end());
+
+        long long res = 0;
+        for (int i = 0; i < k - 1; ++i) {
+            res += possibleCost[n - 2 - i] - possibleCost[i];
         }
-        return maxAns - minAns;
+        return res;
     }
 };
