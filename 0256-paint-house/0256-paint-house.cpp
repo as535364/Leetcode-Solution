5c649b8455efda6826b8a44d871701f9a51cb125
Siao,Yu-Jie <a6194315535@gmail.com>
2025-04-02 18:02:46 +0800

    Time: 0 ms (100%), Space: 13.1 MB (91.96%) - LeetHub

--- 0256-paint-house/0256-paint-house.cpp
+++ 0256-paint-house/0256-paint-house.cpp
@@ -4,14 +4,14 @@ private:
 public:
     int minCost(vector<vector<int>>& costs) {
         int n = costs.size();
-        vector<vector<int>> dp(n, vector<int>(3, inf));
+        vector<vector<int>> dp(2, vector<int>(3, inf));
         dp[0] = costs[0];
 
         for (int i = 1; i < n; ++i) {
-            dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
-            dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
-            dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+            dp[i % 2][0] = min(dp[(i - 1) % 2][1], dp[(i - 1) % 2][2]) + costs[i][0];
+            dp[i % 2][1] = min(dp[(i - 1) % 2][0], dp[(i - 1) % 2][2]) + costs[i][1];
+            dp[i % 2][2] = min(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1]) + costs[i][2];
         }
-        return min({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
+        return min({dp[(n - 1) % 2][0], dp[(n - 1) % 2][1], dp[(n - 1) % 2][2]});
     }
 };
