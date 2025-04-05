19f4a95d19fe9d16f5552619a9daae8860b32158
Siao,Yu-Jie <a6194315535@gmail.com>
2025-04-05 23:32:35 +0800

    Time: 0 ms (100%), Space: 9.3 MB (42.82%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,14 +1,10 @@
 class Solution {
-private:
-    int dfs(int idx, int xorVal, const vector<int> &nums) {
-        if (idx == nums.size()) return xorVal;
-        int sum = 0;
-        sum += dfs(idx + 1, xorVal, nums);
-        sum += dfs(idx + 1, xorVal ^= nums[idx], nums);
-        return sum;
-    }
 public:
     int subsetXORSum(vector<int>& nums) {
-        return dfs(0, 0, nums);
+        int res = 0;
+        for (int num : nums) {
+            res |= num;
+        }
+        return res << (nums.size() - 1);
     }
 };
