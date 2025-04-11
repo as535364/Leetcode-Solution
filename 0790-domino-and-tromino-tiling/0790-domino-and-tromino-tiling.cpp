db5e863b0c6ebbd0ed8b7deff593000cc0f97a16
Siao,Yu-Jie <a6194315535@gmail.com>
2025-04-11 19:15:03 +0800

    Time: 0 ms (100%), Space: 9.1 MB (45.34%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -3,17 +3,21 @@ private:
     const int mod = 1e9 + 7;
 public:
     int numTilings(int n) {
-        vector<int> dp(max(4, n + 1));
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        vector<int> p(max(4, n + 1)), f(max(4, n + 1));
+        p[0] = 1;
+        p[1] = 1;
+        p[2] = 2;
+        p[3] = 5;
+        f[0] = 1;
+        f[1] = 2;
+        f[2] = 4;
+        f[3] = 9;
+
         for (int i = 4; i <= n; ++i) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
-            for (int j = i - 3; j >= 0; --j) {
-                dp[i] = (dp[i] + 2 * dp[j] % mod) % mod;
-            }
+            p[i] = (p[i - 1] + p[i - 2]) % mod;
+            p[i] = (p[i] + 2 * f[i - 3] % mod) % mod;
+            f[i] = (f[i - 1] + p[i]) % mod;
         }
-        return dp[n];
+        return p[n];
     }
 };
