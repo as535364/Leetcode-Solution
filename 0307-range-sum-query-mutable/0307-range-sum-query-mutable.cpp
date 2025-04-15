b3b56507021c19f4d700d06521cbdc25c5c1e0e3
Siao,Yu-Jie <a6194315535@gmail.com>
2025-04-15 21:39:10 +0800

    Time: 44 ms (90.69%), Space: 180.4 MB (80.52%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,30 +1,32 @@
 class NumArray {
 private:
-    vector<int> tree;
+    vector<int> tree, nums;
     int n;
 
     int prefixSum(int index) {
         int res = 0;
-        while (index > 0) {
-            res += tree[index];
-            index -= index & -index;
+        for (int i = index; i > 0; i -= i & -i) {
+            res += tree[i];
         }
         return res;
     }
 public:
-    NumArray(vector<int>& nums): n(nums.size()) {
-        tree.resize(n + 1);
-        for (int i = 0; i < n; ++i) {
-            update(i, nums[i]);
+    NumArray(vector<int>& nums): n(nums.size()), nums(nums), tree(nums.size() + 1) {
+        for (int i = 1; i <= n; ++i) {
+            tree[i] += nums[i - 1];
+            int nxt = i + (i & -i);
+            if (nxt <= n) {
+                tree[nxt] += tree[i];
+            }
         }
     }
     
     void update(int index, int val) {
-        int diff = val - sumRange(index, index);
-        index++;
-        while (index <= n) {
-            tree[index] += diff;
-            index += index & -index;
+        int diff = val - nums[index];
+        nums[index] = val;
+
+        for (int i = index + 1; i <= n; i += i & -i) {
+            tree[i] += diff;
         }
     }
     
