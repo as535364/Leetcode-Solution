1ca416a2571c50fe81916ddd43f939fe2be436a3
Siao,Yu-Jie <a6194315535@gmail.com>
2025-04-28 22:10:59 +0800

    Time: 120 ms (70.33%), Space: 18.4 MB (87.91%) - LeetHub

--- 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
+++ 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
@@ -3,17 +3,17 @@ public:
     int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
         int n = nums.size(), clock = 0;
         vector<int> in(n), out(n), xr(n);
-        vector<vector<int>> g(n);
+        vector<vector<int>> graph(n);
 
         for (const auto &edge : edges) {
-            g[edge[0]].push_back(edge[1]);
-            g[edge[1]].push_back(edge[0]);
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
 
         auto dfs = [&](this auto&& self, int x, int fa) -> void {
             in[x] = ++clock;
             xr[x] = nums[x];
-            for (int neighbor : g[x]) {
+            for (int neighbor : graph[x]) {
                 if (neighbor == fa) continue;
                 self(neighbor, x);
                 xr[x] ^= xr[neighbor];
@@ -21,18 +21,19 @@ public:
             out[x] = clock;
         };
 
-        dfs(0, -1);
-
-
-        auto isAncestor = [&] (int x, int y) -> bool {
+        auto isAncestor = [&](int x, int y){
             return in[x] < in[y] && out[y] <= out[x];
         };
 
+        dfs(0, -1);
+
         int res = numeric_limits<int>::max();
+
         for (int i = 1; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
                 int x, y, z;
-                if (isAncestor(i, j)) { // xr[0] ^ xr[i], xr[i] ^ xr[j], xr[j]
+
+                if (isAncestor(i, j)) {
                     x = xr[0] ^ xr[i], y = xr[i] ^ xr[j], z = xr[j];
                 }
                 else if (isAncestor(j, i)) {
@@ -42,7 +43,6 @@ public:
                     x = xr[0] ^ xr[i] ^ xr[j], y = xr[i], z = xr[j];
                 }
                 res = min(res, max({x, y, z}) - min({x, y, z}));
-                if (res == 0) return 0;
             }
         }
         return res;
