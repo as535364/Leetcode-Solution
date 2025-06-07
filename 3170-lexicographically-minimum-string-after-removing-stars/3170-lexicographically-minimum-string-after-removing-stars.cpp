4562842ccfa14f8740f57afd3e359cffe3cf6341
Siao,Yu-Jie <a6194315535@gmail.com>
2025-06-07 22:13:24 +0800

    Time: 84 ms (66.88%), Space: 41 MB (22.4%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,34 +1,22 @@
 #include <ranges>
-using pci = pair<char, int>;
 class Solution {
 public:
     string clearStars(string s) {
-        priority_queue<pci, vector<pci>, decltype ([](pci &a, pci &b) -> bool {
-            if (a.first != b.first) return a.first > b.first;
-            else return a.second < b.second;
-        })> pq;
-
-        for (auto const [index, c] : views::enumerate(s)) {
-            if (c != '*') pq.push({c, index});
-            else pq.pop();
+        stack<int> stacks[32];
+        for (auto &&[index, c] : views::enumerate(s)) {
+            if (c != '*') {
+                stacks[c - 'a'].push(index);
+                continue;
+            }
+            for (auto &st : stacks) {
+                if (!st.empty()) {
+                    s[st.top()] = '*';
+                    st.pop();
+                    break;
+                }
+            }
         }
-
-        priority_queue<pci, vector<pci>, decltype ([](pci &a, pci &b) -> bool {
-            return a.second > b.second;
-        })> resPQ;
-
-        while (!pq.empty()) {
-            auto [c, index] = pq.top();
-            pq.pop();
-            resPQ.push({c, index});
-        }
-
-        string res;
-        while (!resPQ.empty()) {
-            char c = resPQ.top().first;
-            resPQ.pop();
-            res += c;
-        }
-        return res;
+        s.erase(ranges::remove(s, '*').begin(), s.end());
+        return s;
     }
 };
