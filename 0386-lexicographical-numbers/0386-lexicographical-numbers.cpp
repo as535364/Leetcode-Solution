b60885d487ad61d143d29d4b92747ac9b35512a5
Siao,Yu-Jie <a6194315535@gmail.com>
2025-06-10 15:07:17 +0800

    Time: 0 ms (100%), Space: 13.9 MB (79.88%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        int now = 1;
+
+        for (int i = 0; i < n; ++i) {
+            res.push_back(now);
+
+            if (now * 10 <= n) now *= 10;
+            else {
+                while (now % 10 == 9 || now >= n) now /= 10;
+                now++;
+            }
+        }
+        return res;
+    }
+};
