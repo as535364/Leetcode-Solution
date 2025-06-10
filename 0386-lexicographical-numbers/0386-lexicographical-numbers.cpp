c10ee665918b1318578c5650eba1318e6bb56367
Siao,Yu-Jie <a6194315535@gmail.com>
2025-06-10 15:14:28 +0800

    Time: 3 ms (43.21%), Space: 14 MB (65.53%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,18 +1,30 @@
 class Solution {
+private:
+    void generateLexical(int curr, int limit, vector<int>& res) {
+        if (curr > limit) return;
+        res.push_back(curr);
+        for (int i = 0; i < 10; ++i) {
+            int newNum = curr * 10 + i;
+            if (newNum <= limit) generateLexical(newNum, limit, res);
+        }
+    }
 public:
     vector<int> lexicalOrder(int n) {
         vector<int> res;
-        int now = 1;
-
-        for (int i = 0; i < n; ++i) {
-            res.push_back(now);
-
-            if (now * 10 <= n) now *= 10;
-            else {
-                while (now % 10 == 9 || now >= n) now /= 10;
-                now++;
-            }
+        for (int i = 1; i < 10; ++i) {
+            generateLexical(i, n, res);
         }
+        // int now = 1;
+
+        // for (int i = 0; i < n; ++i) {
+        //     res.push_back(now);
+
+        //     if (now * 10 <= n) now *= 10;
+        //     else {
+        //         while (now % 10 == 9 || now >= n) now /= 10;
+        //         now++;
+        //     }
+        // }
         return res;
     }
 };
