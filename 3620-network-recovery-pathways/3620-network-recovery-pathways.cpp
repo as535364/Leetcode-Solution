1c83cf7c790f9d3ace3b3edc58050ab1e3ae3ab0
Siao,Yu-Jie <a6194315535@gmail.com>
2025-07-21 22:42:54 +0800

    Time: 180 ms (25%), Space: 369.5 MB (48.91%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -1,68 +1,48 @@
 class Solution {
 private:
     const long long inf = 1e15;
-    bool isOK(int threshold, vector<vector<pair<int, int>>>& graph, long long k) {
-        int n = graph.size();
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+    bool isOK(int threshold, vector<vector<pair<int, int>>>& graph, int n, long long k) {
         vector<long long> costs(n, inf);
+        
+        auto dfs = [&](this auto&& self, int x) -> long long {
+            if (x == n - 1) return 0;
+            if (costs[x] != inf) return costs[x];
 
-        pq.push({0, 0});
-        costs[0] = 0;
-
-        while (!pq.empty()) {
-            auto [dis, node] = pq.top();
-            pq.pop();
-            if (costs[node] < dis) continue;
-            if (node == n - 1) return dis <= k;
-
-            for (const auto [neighbor, weight] : graph[node]) {
-                if (threshold > weight) continue;
-                if (costs[neighbor] > weight + dis) {
-                    pq.push({weight + dis, neighbor});
-                    costs[neighbor] = weight + dis;
+            long long res = inf;
+            for (auto [neighbor, weight] : graph[x]) {
+                if (weight >= threshold) {
+                    res = min(res, weight + self(neighbor));
                 }
             }
-        }
-        return false;
+            return costs[x] = res;
+        };
+        return dfs(0) <= k;
     }
 public:
     int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        // 最大最小值 二分搜？
         int n = online.size();
+        int left = 0, right = 0;
         vector<vector<pair<int, int>>> graph(n);
-        // graph[u] {{v1, w1}, {v2, w2}}
-        int left = INT_MAX, right = 0;
+
         for (const auto& edge : edges) {
             int u = edge[0], v = edge[1], w = edge[2];
-            if (online[u] && online[v]) {
-                graph[u].push_back({v, w});
-                left = min(left, w);
-                right = max(right, w);
-            }
+            if (!online[u] || !online[v]) continue;
+            graph[u].push_back({v, w});
+            right = max(right, w);
         }
 
-        bool ans = false;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (isOK(mid, graph, k)) {
+            vector<long long> costs(n, inf);
+            if (isOK(mid, graph, n, k)) {
                 left = mid + 1;
-                ans = true;
             }
             else {
                 right = mid - 1;
             }
         }
-        return ans ? right : -1;
+        return right;
     }
+    // 1 1 1 1 1 0 0 0
 };
-// 1 1 1 1 1 0 0 0 0
-
-// dijkstra O((V+E)logV)
-// 限制： >= threshold
-
-// 10: False
-// 3: True
-
-
-
-// TC: O(log(Diff) * (V+E)logV)
-// SC: O(1 + V + V + E)
