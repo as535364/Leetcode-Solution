b57301dcd027a6b8f8d1d86f7d7961ba1392922e
Siao,Yu-Jie <a6194315535@gmail.com>
2025-08-03 03:36:11 +0800

    Time: 4 ms (83.33%), Space: 238.2 MB (83.33%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -3,18 +3,16 @@ public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int n = landStartTime.size(), m = waterStartTime.size();
         int minLand = INT_MAX, minWater = INT_MAX;
+        int landWater = INT_MAX, waterLand = INT_MAX;
 
         for (int i = 0; i < n; ++i) {
             minLand = min(minLand, landStartTime[i] + landDuration[i]);
         }
         for (int i = 0; i < m; ++i) {
+            landWater = min(landWater, max(minLand, waterStartTime[i]) + waterDuration[i]);
             minWater = min(minWater, waterStartTime[i] + waterDuration[i]);
         }
 
-        int landWater = INT_MAX, waterLand = INT_MAX;
-        for (int i = 0; i < m; ++i) {
-            landWater = min(landWater, max(minLand, waterStartTime[i]) + waterDuration[i]);
-        }
         for (int i = 0; i < n; ++i) {
             waterLand = min(waterLand, max(minWater, landStartTime[i]) + landDuration[i]);
         }
