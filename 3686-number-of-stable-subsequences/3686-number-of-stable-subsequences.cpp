d1e983e2c5e42bdd390a22ade1ba9db58a06c156
Siao,Yu-Jie <a6194315535@gmail.com>
2025-09-15 22:47:55 +0800

    Time: 738 ms (8.33%), Space: 395.2 MB (31.17%) - LeetHub

--- 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
+++ 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
@@ -4,11 +4,17 @@ private:
 public:
     int countStableSubsequences(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(2)));
+        vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(2, vector<long long>(2)));
         // 前 i 個, 最後一個奇偶, 連續幾個奇偶
 
         for (int i = 1; i <= n; ++i) {
-           int x = nums[i - 1];
+           int x = nums[i - 1] % 2;
+           dp[i][x][0] = (dp[i - 1][x][0] + dp[i - 1][x ^ 1][0] + dp[i - 1][x ^ 1][1] + 1) % mod;
+           dp[i][x][1] = (dp[i - 1][x][1] + dp[i - 1][x][0]) % mod;
+
+           dp[i][x ^ 1][0] = dp[i-1][x ^ 1][0];
+           dp[i][x ^ 1][1] = dp[i-1][x ^ 1][1];
         }
+        return (dp[n][0][0] + dp[n][1][0] + dp[n][0][1] + dp[n][1][1]) % mod;
     }
 };
