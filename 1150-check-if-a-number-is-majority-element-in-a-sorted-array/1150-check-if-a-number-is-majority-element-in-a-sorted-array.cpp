b09a53f7a9da7bcd03be03ef1f450698047dc2d9
Siao,Yu-Jie <a6194315535@gmail.com>
2025-09-18 15:37:46 +0800

    Time: 0 ms (100%), Space: 11.1 MB (46.38%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isMajorityElement(vector<int>& nums, int target) {
-        auto itStart = lower_bound(nums.begin(), nums.end(), target);
-        auto itEnd = upper_bound(nums.begin(), nums.end(), target);
-        return itEnd - itStart > nums.size() / 2;
+        int idxStart = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return idxStart + nums.size() / 2 < nums.size() && nums[idxStart + nums.size() / 2] == target;
     }
 };
