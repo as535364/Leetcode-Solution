86813c006db88e65463e9beb9ce9d4184e316f14
Siao,Yu-Jie <a6194315535@gmail.com>
2025-10-06 19:46:00 +0800

    Time: 75 ms (98.55%), Space: 36.8 MB (98.97%) - LeetHub

--- 3694-distinct-points-reachable-after-substring-removal/3694-distinct-points-reachable-after-substring-removal.cpp
+++ 3694-distinct-points-reachable-after-substring-removal/3694-distinct-points-reachable-after-substring-removal.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int distinctPoints(string s, int k) {
-        int x = 0, y = 0;
-        set<pair<int, int>> seen;
-        seen.insert({0, 0});
+        // int x = 0, y = 0;
+        // set<pair<int, int>> seen;
+        // seen.insert({0, 0});
+        long long x = 0, y = 0, n = s.size();
+        unordered_set<long long> seen;
+        seen.insert((x + n) << 32 | (y + n));
         for (int i = k; i < s.size(); ++i) {
             switch (s[i]) {
                 case 'U': ++y; break;
@@ -18,7 +21,7 @@ public:
                 case 'L': ++x; break;
                 case 'R': --x; break;
             }
-            seen.insert({x, y});
+            seen.insert((x + n) << 32 | (y + n));
         }
         return seen.size();
     }
