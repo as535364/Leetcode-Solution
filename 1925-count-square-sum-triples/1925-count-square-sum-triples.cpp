e7b0623fd44c0b13e4d157502cae7b78f7a533bc
Siao,Yu-Jie <a6194315535@gmail.com>
2025-12-08 15:56:15 +0800

    Time: 1 ms (95.13%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,10 +4,9 @@ public:
         int res = 0;
         for (int i = 1; i <= n; ++i) {
             for (int j = i + 1; j <= n; ++j) {
-                for (int k = j + 1; k <= n; ++k) {
-                    if (i * i + j * j == k * k) {
-                        ++res;
-                    }
+                int k = sqrt(i * i + j * j);
+                if (k <= n && i * i + j * j == k * k) {
+                    ++res;
                 }
             }
         }
