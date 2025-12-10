e50963fa8b606754b49a70313dac429275d7f70b
Siao,Yu-Jie <a6194315535@gmail.com>
2025-12-11 00:50:39 +0800

    Time: 45 ms (57.49%), Space: 66.9 MB (77.15%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         auto cmp = [](const pair<int, int> &a, const pair<int, int> &b) {
-            return a.first * a.first + a.second * a.second > b.first * b.first + b.second * b.second;
+            return a.first * a.first + a.second * a.second < b.first * b.first + b.second * b.second;
         };
 
         priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq;
 
         for (const auto& point : points) {
             pq.push({point[0], point[1]});
+            if (pq.size() > k) pq.pop();
         }
 
         vector<vector<int>> res;
