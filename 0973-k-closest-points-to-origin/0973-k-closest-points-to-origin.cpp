847cf6371e97552af38ba52c608ac765cd27c2ac
Siao,Yu-Jie <a6194315535@gmail.com>
2025-12-11 00:51:09 +0800

    Time: 210 ms (8.73%), Space: 81.2 MB (25.11%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        auto cmp = [](const pair<int, int> &a, const pair<int, int> &b) {
-            return a.first * a.first + a.second * a.second < b.first * b.first + b.second * b.second;
-        };
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq;
-
-        for (const auto& point : points) {
-            pq.push({point[0], point[1]});
-            if (pq.size() > k) pq.pop();
+        priority_queue<vector<int>> pq;
+        for(auto point : points){
+            int x = point[0], y = point[1];
+            int disSquare = x * x + y * y;
+            pq.push({disSquare, x, y});
+            if(pq.size() > k)pq.pop();
         }
-
         vector<vector<int>> res;
-        while (k--) {
-            res.push_back({pq.top().first, pq.top().second});
+        while(!pq.empty()){
+            int x = pq.top()[1], y = pq.top()[2];
             pq.pop();
+            res.push_back({x, y});
         }
         return res;
     }
