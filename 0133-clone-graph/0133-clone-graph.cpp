068a953625e9eeeb4ca6f38b5229906caeda453b
Siao,Yu-Jie <a6194315535@gmail.com>
2025-12-17 18:42:23 +0800

    Time: 0 ms (100%), Space: 10.9 MB (99.97%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -20,45 +20,41 @@ public:
 */
 
 class Solution {
+private:
+    unordered_map<Node*, Node*> vis;
 public:
     Node* cloneGraph(Node* node) {
-        if (node == nullptr) return nullptr;
+        if (node == nullptr) return node;
+        stack<Node*> st;
+        st.push(node);
+        vis[node] = new Node(node -> val);
 
-        queue<Node *> q;
-        unordered_map<int, Node*> node_map;
-        unordered_set<int> visit;
+        while (!st.empty()) {
+            Node *nowNode = st.top(), *newNode = nullptr;
+            st.pop();
+            auto it = vis.find(nowNode);
+            if (it == vis.end()) {
+                newNode = new Node(nowNode -> val);
+            }
+            else {
+                newNode = it -> second;
+            }
 
-        Node *res_node = new Node(node -> val);
-        node_map[node -> val] = res_node;
-        visit.insert(node -> val);
-        q.push(node);
-
-        while (!q.empty()) {
-            Node *curr_node = q.front();
-            q.pop();
-            cout << curr_node -> val << endl;
-
-            Node *copy_node = nullptr;
-            auto it = node_map.find(curr_node -> val);
-            copy_node = it -> second;
-
-            for (Node* neighbor_node : curr_node -> neighbors) {
-                Node *copy_neighbor_node = nullptr;
-                auto it = node_map.find(neighbor_node -> val);
-                if (it == node_map.end()) {
-                    copy_neighbor_node = new Node(neighbor_node -> val);
-                    node_map[neighbor_node -> val] = copy_neighbor_node;
+            for (Node *neighbor : nowNode -> neighbors) {
+                cout << neighbor -> val << " ";
+                Node *newNeighborNode = nullptr;
+                auto newNeighborNodeIt = vis.find(neighbor);
+                if (newNeighborNodeIt == vis.end()) {
+                    newNeighborNode = new Node(neighbor -> val);
+                    vis[neighbor] = newNeighborNode;
+                    st.push(neighbor);
                 }
                 else {
-                    copy_neighbor_node = it -> second;
+                    newNeighborNode = newNeighborNodeIt -> second;
                 }
-                copy_node -> neighbors.push_back(copy_neighbor_node);
-
-                if (visit.find(neighbor_node -> val) != visit.end()) continue;
-                visit.insert(neighbor_node -> val);
-                q.push(neighbor_node);
+                newNode -> neighbors.push_back(newNeighborNode);
             }
         }
-        return res_node;
+        return vis[node];
     }
 };
